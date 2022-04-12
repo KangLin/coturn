93545db30b73fafbdb446ceeca28b96f4a9b97e2
Kang Lin <kl222@126.com>
2022-04-12 20:54:25 +0800

    Windows: Get local ip address

--- src/apps/relay/mainrelay.c
+++ src/apps/relay/mainrelay.c
@@ -35,6 +35,15 @@
 #include "prom_server.h"
 #endif
 
+#ifdef MSVC
+    #include <Iphlpapi.h>
+
+    #define WORKING_BUFFER_SIZE 15000
+    #define MAX_TRIES 3
+
+    #define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
+    #define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
+#endif
 
 #if (defined LIBRESSL_VERSION_NUMBER && OPENSSL_VERSION_NUMBER == 0x20000000L)
 #undef OPENSSL_VERSION_NUMBER
@@ -228,8 +237,219 @@ static int make_local_listeners_list(void)
 {
 	int ret = 0;
 #ifdef MSVC
-	//TODO: implement it
-	TURN_LOG_FUNC(TURN_LOG_LEVEL_WARNING, "Don't implement make_local_listeners_list in msvc");
+
+    DWORD dwSize = 0;
+    DWORD dwRetVal = 0;
+
+    unsigned int i = 0;
+
+    // Set the flags to pass to GetAdaptersAddresses
+    ULONG flags = GAA_FLAG_INCLUDE_PREFIX;
+
+    // default to unspecified address family (both)
+    ULONG family = AF_UNSPEC;
+
+    LPVOID lpMsgBuf = NULL;
+
+    PIP_ADAPTER_ADDRESSES pAddresses = NULL;
+    ULONG outBufLen = 0;
+    ULONG Iterations = 0;
+
+    PIP_ADAPTER_ADDRESSES pCurrAddresses = NULL;
+    PIP_ADAPTER_UNICAST_ADDRESS pUnicast = NULL;
+    PIP_ADAPTER_ANYCAST_ADDRESS pAnycast = NULL;
+    PIP_ADAPTER_MULTICAST_ADDRESS pMulticast = NULL;
+    IP_ADAPTER_DNS_SERVER_ADDRESS *pDnServer = NULL;
+    IP_ADAPTER_PREFIX *pPrefix = NULL;
+
+    // Allocate a 15 KB buffer to start with.
+    outBufLen = WORKING_BUFFER_SIZE;
+
+    do {
+
+        pAddresses = (IP_ADAPTER_ADDRESSES *)MALLOC(outBufLen);
+        if (pAddresses == NULL) {
+            TURN_LOG_FUNC(TURN_LOG_LEVEL_ERROR,
+                "Memory allocation failed for IP_ADAPTER_ADDRESSES struct\n");
+            return -1;
+        }
+
+        dwRetVal =
+            GetAdaptersAddresses(family, flags, NULL, pAddresses, &outBufLen);
+
+        if (dwRetVal == ERROR_BUFFER_OVERFLOW) {
+            FREE(pAddresses);
+            pAddresses = NULL;
+        }
+        else {
+            break;
+        }
+
+        Iterations++;
+
+    } while ((dwRetVal == ERROR_BUFFER_OVERFLOW) && (Iterations < MAX_TRIES));
+
+    if (dwRetVal == NO_ERROR) {
+        // If successful, output some information from the data we received
+        pCurrAddresses = pAddresses;
+        while (pCurrAddresses) {
+            /*
+            printf("\tLength of the IP_ADAPTER_ADDRESS struct: %ld\n",
+                pCurrAddresses->Length);
+            printf("\tIfIndex (IPv4 interface): %u\n", pCurrAddresses->IfIndex);
+            printf("\tAdapter name: %s\n", pCurrAddresses->AdapterName);//*/
+
+            pUnicast = pCurrAddresses->FirstUnicastAddress;
+            if (pUnicast != NULL) {
+                //printf("\tNumber of Unicast Addresses:\n");
+                for (i = 0; pUnicast != NULL; pUnicast = pUnicast->Next)
+                {
+                    char saddr[INET6_ADDRSTRLEN] = "";
+                    if (AF_INET == pUnicast->Address.lpSockaddr->sa_family) // IPV
+                        inet_ntop(PF_INET, &((struct sockaddr_in*)pUnicast->Address.lpSockaddr)->sin_addr, saddr, INET6_ADDRSTRLEN);
+                    else if (AF_INET6 == pUnicast->Address.lpSockaddr->sa_family) // IPV6
+                        inet_ntop(PF_INET6, &((struct sockaddr_in6*)pUnicast->Address.lpSockaddr)->sin6_addr, saddr, INET6_ADDRSTRLEN);
+                    else
+                        continue;
+
+                    //printf("\t\tIP: %s\n", saddr);
+                    if (strstr(saddr, "169.254.") == saddr)
+                        continue;
+                    if (!strcmp(saddr, "0.0.0.0"))
+                        continue;
+                    if (strstr(saddr, "fe80") == saddr)
+                        continue;
+                    if (!strcmp(saddr, "::"))
+                        continue;
+
+                    add_listener_addr(saddr);
+
+                    if (MIB_IF_TYPE_LOOPBACK != pCurrAddresses->IfType)
+                        ret++;
+                }
+            }
+            /*
+            else
+                printf("\tNo Unicast Addresses\n");
+
+            pAnycast = pCurrAddresses->FirstAnycastAddress;
+            if (pAnycast) {
+                for (i = 0; pAnycast != NULL; i++)
+                    pAnycast = pAnycast->Next;
+                printf("\tNumber of Anycast Addresses: %d\n", i);
+            }
+            else
+                printf("\tNo Anycast Addresses\n");
+
+            pMulticast = pCurrAddresses->FirstMulticastAddress;
+            if (pMulticast) {
+                for (i = 0; pMulticast != NULL; i++)
+                    pMulticast = pMulticast->Next;
+                printf("\tNumber of Multicast Addresses: %d\n", i);
+            }
+            else
+                printf("\tNo Multicast Addresses\n");
+
+            pDnServer = pCurrAddresses->FirstDnsServerAddress;
+            if (pDnServer) {
+                for (i = 0; pDnServer != NULL; i++)
+                    pDnServer = pDnServer->Next;
+                printf("\tNumber of DNS Server Addresses: %d\n", i);
+            }
+            else
+                printf("\tNo DNS Server Addresses\n");
+
+            printf("\tDNS Suffix: %wS\n", pCurrAddresses->DnsSuffix);
+            printf("\tDescription: %wS\n", pCurrAddresses->Description);
+            printf("\tFriendly name: %wS\n", pCurrAddresses->FriendlyName);
+
+            if (pCurrAddresses->PhysicalAddressLength != 0) {
+                printf("\tPhysical address: ");
+                for (i = 0; i < (int)pCurrAddresses->PhysicalAddressLength;
+                    i++) {
+                    if (i == (pCurrAddresses->PhysicalAddressLength - 1))
+                        printf("%.2X\n",
+                        (int)pCurrAddresses->PhysicalAddress[i]);
+                    else
+                        printf("%.2X-",
+                        (int)pCurrAddresses->PhysicalAddress[i]);
+                }
+            }
+            printf("\tFlags: %ld\n", pCurrAddresses->Flags);
+            printf("\tMtu: %lu\n", pCurrAddresses->Mtu);
+            char* pType = NULL;
+            
+            switch (pCurrAddresses->IfType)
+            {
+            case MIB_IF_TYPE_ETHERNET:
+                pType = "ETHERNET";
+                break;
+            case MIB_IF_TYPE_PPP:
+                pType = "PPP";
+                break;
+            case MIB_IF_TYPE_LOOPBACK:
+                pType = "LOOPBACK";
+                break;
+            case MIB_IF_TYPE_SLIP:
+                pType = "ATM";
+                break;
+            case IF_TYPE_IEEE80211:
+                pType = "WIFI";
+                break;
+            }
+            printf("\tIfType: %ld (%s)\n", pCurrAddresses->IfType, pType);
+            printf("\tOperStatus: %ld\n", pCurrAddresses->OperStatus);
+            printf("\tIpv6IfIndex (IPv6 interface): %u\n",
+                pCurrAddresses->Ipv6IfIndex);
+            printf("\tZoneIndices (hex): ");
+            for (i = 0; i < 16; i++)
+                printf("%lx ", pCurrAddresses->ZoneIndices[i]);
+            printf("\n");
+
+            printf("\tTransmit link speed: %I64u\n", pCurrAddresses->TransmitLinkSpeed);
+            printf("\tReceive link speed: %I64u\n", pCurrAddresses->ReceiveLinkSpeed);
+
+            pPrefix = pCurrAddresses->FirstPrefix;
+            if (pPrefix) {
+                for (i = 0; pPrefix != NULL; i++)
+                    pPrefix = pPrefix->Next;
+                printf("\tNumber of IP Adapter Prefix entries: %d\n", i);
+            }
+            else
+                printf("\tNumber of IP Adapter Prefix entries: 0\n");
+
+            printf("\n");//*/
+
+            pCurrAddresses = pCurrAddresses->Next;
+        }
+    }
+    else {
+        TURN_LOG_FUNC(TURN_LOG_LEVEL_ERROR,
+            "Call to GetAdaptersAddresses failed with error: %d\n",
+            dwRetVal);
+        if (dwRetVal == ERROR_NO_DATA)
+            TURN_LOG_FUNC(TURN_LOG_LEVEL_ERROR,
+                "\tNo addresses were found for the requested parameters\n");
+        else {
+
+            if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL, dwRetVal, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                // Default language
+                (LPTSTR)& lpMsgBuf, 0, NULL)) {
+                TURN_LOG_FUNC(TURN_LOG_LEVEL_ERROR, "\tError: %s", lpMsgBuf);
+                LocalFree(lpMsgBuf);
+                if (pAddresses)
+                    FREE(pAddresses);
+                return -2;
+            }
+        }
+    }
+
+    if (pAddresses) {
+        FREE(pAddresses);
+    }
+
 #else
 	struct ifaddrs * ifs = NULL;
 	struct ifaddrs * ifa = NULL;
